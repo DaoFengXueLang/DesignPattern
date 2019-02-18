898b7e5349b7bd91198554205e0cfecaed9cd45a
DaoFengXueLang <120583174@qq.com>
2019-02-18 22:17:47 +0800

    111
    Signed-off-by: DaoFengXueLang <120583174@qq.com>

--- src/CreatingPattern/Bulider/Bottle.java
+++ src/CreatingPattern/Bulider/Bottle.java
@@ -1,9 +1,9 @@
-package CreatingPattern.Bulider;
+﻿package CreatingPattern.Bulider;
 
 public class Bottle implements Packing{
     //商品的包装种类之一，瓶装
     @Override
     public String pack() {
-        return "Bottle!";
+         return "Bottle!";
     }
 }
